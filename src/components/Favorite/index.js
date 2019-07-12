0b1d0a0fe69b357cefef4b9b79efbb9509ada9e1
Will Sheehan <will@citizenhex.com>
2019-07-12 13:36:56 -0400

    favorite component

---
+++ src/components/Favorite/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar as faStarReg } from '@fortawesome/free-regular-svg-icons'
+import './style.scss'
+
+function Favorite({ user, asset, actions: { toggleFav } }) {
+    if (user.favorites.includes(asset.asset)) {
+        return <div className="favorite"><FontAwesomeIcon icon={faStar} onClick={() => toggleFav(asset.asset)} /></div>
+    } else {
+        return <div className="favorite"><FontAwesomeIcon icon={faStarReg} onClick={() => toggleFav(asset.asset)} /></div>
+    }
+}
+
+export default Favorite
