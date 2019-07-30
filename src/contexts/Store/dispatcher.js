17017e9947d07979350d4f6bc7775f49be30e881
Brett <brett@citizenhex.com>
2019-07-29 23:01:43 -0400

    remap private.myCommands to be keyed by transactionHash

--- src/contexts/Store/dispatcher.js
+++ src/contexts/Store/dispatcher.js
@@ -1,4 +1,4 @@
-import { set, uniqueId } from 'lodash'
+import { set, uniqueId, keyBy } from 'lodash'
 import { BN, unlimitedAllowance } from '../../utils'
 import Selectors from '../../utils/selectors'
 
@@ -43,9 +43,58 @@ function actionGenerator (type, params, resp) {
   }
 }
 
+/*
+* Remaps `private.myCommand` commands to be keyed by transactionHash if available
+*/
+function remapPrivateData (action) {
+  let { path, data } = action.params
+  if (typeof path === 'string') path = path.split('.')
+  // ['private']
+  if (path.length === 1) {
+    if (data.myCommands) {
+      data.myCommands = keyBy(
+        Object.values(data.myCommands),
+        command => command.transactionHash || command.id
+      )
+    }
+  }
+  // ['private', 'myCommands']
+  if (path.length === 2 && path[1] === 'myCommands') {
+    data = keyBy(
+      Object.values(data),
+      command => command.transactionHash || command.id
+    )
+  }
+
+  // ['private', 'myCommands', 'id123']
+  if (path.length === 3 && path[1] === 'myCommands') {
+    path[2] = data.transactionHash || data.id
+  }
+
+  return {
+    ...action,
+    params: {
+      ...action.params,
+      path,
+      data
+    }
+  }
+}
+
+function isPrivateUpdate (action) {
+  return (
+    (Array.isArray(action.params.path) &&
+      action.params.path[0] === 'private') ||
+    /^private/.test(action.params.path)
+  )
+}
+
 export function reducer (state, action) {
   switch (action.type) {
     case 'UPDATE':
+      if (isPrivateUpdate) {
+        action = remapPrivateData(action)
+      }
       console.log(action.params.path, action.params.data)
       return { ...set(state, action.params.path, action.params.data) }
     case 'ERROR': {
