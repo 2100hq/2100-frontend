906e3d3fb47a5fb1a67a20d9fb8271d2babf655e
Brett <brett@citizenhex.com>
2019-07-29 22:55:29 -0400

    use array paths

--- src/utils/selectors.js
+++ src/utils/selectors.js
@@ -3,14 +3,22 @@ import { get } from 'lodash'
 export default function Selectors (state) {
   const controller = {}
   controller.contract = get(state, 'contracts.controller')
-  controller.walletPath = `private.myWallets.available.${
+  controller.walletPath = [
+    'private',
+    'myWallets',
+    'available',
     (controller.contract || {}).address
-  }`
+  ]
   controller.wallet = get(state, controller.walletPath, {})
 
   const dai = {}
   dai.contract = get(state, 'contracts.dai')
-  dai.walletPath = `private.myWallets.available.${(dai.contract || {}).address}`
+  dai.walletPath = [
+    'private',
+    'myWallets',
+    'available',
+    (dai.contract || {}).address
+  ]
   dai.wallet = get(state, dai.walletPath, {})
 
   return {
