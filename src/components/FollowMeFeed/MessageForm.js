946e8e91e8a7bc448b644430302c5229ef3f4fd5
Brett <brett@citizenhex.com>
2019-08-21 12:08:26 -0400

    FollowMe message bar refactored; faster; reversed dots

--- src/components/FollowMeFeed/MessageForm.js
+++ src/components/FollowMeFeed/MessageForm.js
@@ -19,11 +19,11 @@ export default function MessageForm({myTokenName}){
   const [message, setMessage] = useState('')
   const [error, setError] = useState()
   const [level, setLevel] = useState(4)
-  const [threshold, setThreshold] = useState(0)
+  const [threshold, setThreshold] = useState("1")
   const [recipientCount, setRecipientCount] = useState(0)
 
   const hasToken = myToken != null
-  const hasNoFollowers = followerCount === 0
+  const hasFollowers = myToken && followerCount > 0
 
   const isDisabled = !hasToken || submitting
 
@@ -48,15 +48,35 @@ export default function MessageForm({myTokenName}){
   }
 
   useEffect( () => {
-    if (hasNoFollowers) return setThreshold("1")
+    let newThresh = "1"
+    let swappedLevel = 4-level
     const holdings = Object.values(followers)
-    const p = percentiles[level]
-    const newThresh = percentile(holdings, p)
-    setThreshold(newThresh.toString())
-    setRecipientCount(holdings.filter( amount => {
-      return BigNumber(amount).gte(threshold)
-    }).length)
+    if (hasFollowers && 4-level>0){
+      const p = percentiles[4-level]
+      newThresh = percentile(holdings, p).toString()
+    }
+    setThreshold(newThresh)
   }, [level, followers])
+
+  useEffect( () => {
+    const holdings = Object.values(followers)
+    let count = hasFollowers ? holdings.length : 0
+
+    if (threshold != null && threshold !== "1"){
+      count = holdings.filter( amount => {
+        return BigNumber(amount).gte(threshold)
+      }).length
+    }
+
+    setRecipientCount(count)
+
+  }, [threshold, followers])
+
+  const tokenRequirement = (
+    <div className="small">
+      Need {threshold === "1" ? 'some' : toDecimals(threshold,3,1)} ${myTokenName}
+    </div>
+  )
 
   return (
       <div className='card'>
@@ -68,7 +88,7 @@ export default function MessageForm({myTokenName}){
           <div className='clearfix'>
             <div className='float-left'>
               <Dots current={level} onClick={handleSetLevel} isDisabled={isDisabled}/>
-              <div className="text-muted small"><i className='fas fa-eye' /> {recipientCount} holders { hasToken && `(${toDecimals(threshold,3,1)} $${myTokenName})`}</div>
+              <div className="text-muted small"><i className='fas fa-eye' /> {recipientCount > 0 && threshold === "1" && 'All'} {recipientCount} holders { hasToken && tokenRequirement }</div>
             </div>
             <div className='float-right'>
               <Button variant="primary" type="submit" disabled={isDisabled || isEmpty(message) ? 'disabled' : null}>
