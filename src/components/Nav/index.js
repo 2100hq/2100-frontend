0d5d96d38883236b95ae2e20d9def407528523ac
ben <ben@citizenhex.com>
2019-08-22 13:03:58 -0400

    initial structure

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -12,10 +12,10 @@ function ProtectedNavItem ({state, children}) {
 export default function Nav (props) {
   const { state } = useStoreContext()
   return (
-    <nav className='navbar navbar-expand-lg navbar-light'>
-      <Link to='/' className='navbar-brand'>
-        2100
-      </Link>
+    <div style={{backgroundColor: 'black'}}>
+    <nav className='navbar navbar-expand-lg navbar-dark'>
+      <Link to='/' className='navbar-brand'>2100</Link>
+      
       <button
         className='navbar-toggler'
         type='button'
@@ -27,6 +27,7 @@ export default function Nav (props) {
       >
         <span className='navbar-toggler-icon' />
       </button>
+
       <div className='collapse navbar-collapse' id='navbarTogglerDemo03'>
         <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
 
@@ -37,5 +38,17 @@ export default function Nav (props) {
         </ul>
       </div>
     </nav>
+      <div style={{color: 'white'}}>
+        <div style={{minHeight: '8rem'}} className="row align-items-center justify-content-center">
+          <div className="col-md-4 text-center">
+<div><img src='../img/dai.png' style={{ width: '20px','vertical-align': 'middle' }} />123 minting</div>
+            <h3>$benjmnr2</h3>
+          </div>
+        </div>
+        <div className="row">
+        </div>
+      </div>
+
+    </div>
   )
 }
