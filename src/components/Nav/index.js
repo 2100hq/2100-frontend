14f0d0328888674dbc60edf70839f66f28b50b4c
Brett <brett@citizenhex.com>
2019-08-23 21:18:33 -0400

    real token name/stakes in nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useStoreContext } from '../../contexts/Store'
+import { toDecimals } from '../../utils'
 import User from './User'
 import './style.scss'
 
@@ -9,8 +10,22 @@ function ProtectedNavItem ({state, children}) {
     return children
 }
 
+function MiniProfile(){
+  const { query } = useStoreContext()
+  const isSignedIn = query.getIsSignedIn()
+  if (!isSignedIn) return null
+  const myToken = query.getMyToken()
+  if (!myToken || !myToken.id) return null
+  return (
+    <div className="col-md-4 text-center">
+      <div><img src='../img/dai.png' style={{ width: '20px','vertical-align': 'middle' }} />{toDecimals(myToken.totalStakes,5)} minting</div>
+      <h3><span class='token-name'>{myToken.name}</span></h3>
+    </div>
+  )
+}
+
 export default function Nav (props) {
-  const { state } = useStoreContext()
+  const { state, query } = useStoreContext()
   return (
     <div className="header-background">
       <nav className='navbar navbar-expand-lg navbar-dark'>
@@ -36,10 +51,7 @@ export default function Nav (props) {
       </nav>
       <div style={{color: 'white'}}>
         <div style={{minHeight: '9rem'}} className="row align-items-center justify-content-center">
-          <div className="col-md-4 text-center">
-            <div><img src='../img/dai.png' style={{ width: '20px','vertical-align': 'middle' }} />123 minting</div>
-            <h3>$benjmnr2</h3>
-          </div>
+          <MiniProfile />
         </div>
       </div>
     </div>
