7add166920f1a5e76c90264886bc69358681ea7a
Brett <brett@citizenhex.com>
2019-08-23 20:47:11 -0400

    added sent messages to public feed

--- src/components/FollowMe/PublicFeed.js
+++ src/components/FollowMe/PublicFeed.js
@@ -6,8 +6,8 @@ import FollowMe from './'
 export default function PublicFeed(){
   const {query} = useStoreContext()
   const isSignedIn = query.getIsSignedIn()
-  const { publicMessages, privateMessages, decodedMessages } = useFollowMeContext()
-  const messages = { ...publicMessages, ...privateMessages, ...decodedMessages }
+  const { publicMessages, sentMessages, privateMessages, decodedMessages } = useFollowMeContext()
+  const messages = { ...publicMessages, ...privateMessages, ...decodedMessages, ...sentMessages }
 
   return <FollowMe messages={messages} showForm={isSignedIn} className='public-feed'/>
 }
