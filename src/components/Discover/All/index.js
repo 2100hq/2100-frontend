3e1d5258244e620f8ef51c013f65cedbc0111bf6
Brett <brett@citizenhex.com>
2019-08-28 12:22:53 -0400

    Count Up everywhere on Discover; stake direction arrows; stakers number with icon

--- src/components/Discover/All/index.js
+++ src/components/Discover/All/index.js
@@ -1,17 +1,18 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState,useEffect, useRef } from 'react'
 import { useStoreContext } from '../../../contexts/Store'
 import { toDecimals, BigNumber, weiDecimals } from '../../../utils'
 import Allocator from '../../Allocator'
 import { useCountUp } from 'react-countup'
 import { sortBy } from 'lodash'
 import { Link } from 'react-router-dom'
-function BalanceCountUp ({token}) {
-  const balance = toDecimals(token.balances.available,5)
+import './style.scss'
+
+function CountUp ({balance, decimals = 5}) {
   const { countUp, update } = useCountUp({
     start: 0,
     end: balance,
     delay: 0,
-    decimals: 5,
+    decimals,
     duration: 0.25
   })
   useEffect(() => {
@@ -22,6 +23,8 @@ function BalanceCountUp ({token}) {
 
 
 function Row ({ rank, token, myToken }) {
+  const prevTotalStakeRef = useRef(token.totalStakes)
+  const [stakeArrowDirection, setStakeArrowDirection]=useState(null)
   let earning = null
 
   if (BigNumber(token.myStake).gt(0)){
@@ -37,6 +40,16 @@ function Row ({ rank, token, myToken }) {
   const showEarning = earning != null
   if (showEarning) earning = earning.dp(6,1).toString()
 
+  const stakers = Object.values(token.stakes || {}).filter( stake => BigNumber(stake).gt(0) ).length
+
+  useEffect( () => {
+    if (prevTotalStakeRef.current === token.totalStakes) return setStakeArrowDirection(null)
+    const direction = BigNumber(prevTotalStakeRef.current).lt(token.totalStakes) ? 'up' : 'down'
+    setStakeArrowDirection(direction)
+    prevTotalStakeRef.current=token.totalStakes
+    // const id = setTimeout(setStakeArrowDirection, 5000, null)
+    // return () => clearTimeout(id)
+  }, [token.totalStakes])
   return (
     <tr>
       <th scope='row'>{rank}
@@ -46,14 +59,18 @@ function Row ({ rank, token, myToken }) {
         <div className='token-description' style={{ fontSize: '0.7rem',color:'#aaa'}}>Access my new sci-fi story ...</div>
       </td>
       <td>
-        <span className='text-muted'>{toDecimals(token.totalStakes)} DAI</span>
+        <span className='text-muted' style={{position: 'relative'}}>
+          { stakeArrowDirection && <i className={`fas fa-arrow-${stakeArrowDirection} stake-arrow`}></i> }
+          <CountUp balance={toDecimals(token.totalStakes)} decimals={2} /> DAI
+        </span>
+        <div className='small text-muted'>{stakers > 0 && <span><i className="fas fa-user stakers-icon"></i> <CountUp balance={stakers} decimals={0} /></span>}</div>
       </td>
       <td>
-        <Allocator token={token} /><span className="small">{toDecimals(token.myStake)} DAI</span>
+        <Allocator token={token} /><span className="small"><CountUp balance={toDecimals(token.myStake)} decimals={2} /> DAI</span>
       </td>
-      <td>{ showEarning && <span>{earning} <span className='text-muted'>${token.name}</span></span> }</td>
+      <td>{ showEarning && <span><CountUp balance={earning} decimals={6} /> <span className='text-muted'>${token.name}</span></span> }</td>
       <td>
-        <span className='small'><i class="fas fa-coins"></i></span> <BalanceCountUp token={token} />
+        <span className='small'><i className="fas fa-coins"></i></span> <CountUp balance={toDecimals(token.balances.available,5)} />
       </td>
     </tr>
   )
