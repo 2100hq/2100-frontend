3d6e1975dd21cb7374e5218d4103cada03c6229f
Brett <brett@citizenhex.com>
2019-09-03 16:35:24 -0400

    CreateMessageButton is just an icon

--- src/components/FollowMe/CreateMessageButton/index.js
+++ src/components/FollowMe/CreateMessageButton/index.js
@@ -2,13 +2,15 @@ import React, {useState} from 'react'
 import {Button} from 'react-bootstrap'
 import { useStoreContext } from '../../../contexts/Store'
 import CreateMessageModal from '../CreateMessageModal'
+import './style.scss'
+
 export default function CreateMessageButton(props){
   const [show, setShow] = useState(false)
 
   return (
     <>
     <CreateMessageModal show={show} onHide={()=>setShow(false)} />
-    <Button {...props} onClick={()=>setShow(true)}>Create</Button>
+    <Button {...props}  variant="dark" onClick={()=>setShow(true)}><i class="fas fa-pen"></i></Button>
     </>
   )
 }
