9e23c543fe708d13aff828a457c91a583024dd2f
ben <ben@citizenhex.com>
2019-09-05 13:04:29 -0400

    sidebar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PublicFeed from '../FollowMe/PublicFeed'
+import './style.scss'
+
+
+export default function Sidebar (props) {
+  return (
+  	<div>
+  		<div className='public-header'>
+  			
+  		</div>
+	  	<div className='public-sub-header row'>
+			<ul className="nav nav-pills">
+			  <li className="nav-item">
+			    <a className="nav-link active" href="#">All</a>
+			  </li>
+			  <li className="nav-item">
+			    <a className="nav-link" href="#">Staking</a>
+			  </li>
+			</ul>
+	  	</div>
+	  	<PublicFeed />
+  	</div>
+    )
+}
