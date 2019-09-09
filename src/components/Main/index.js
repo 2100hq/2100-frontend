0a1d007067bba5b81ebdd289aa53e5a600348f02
Brett <brett@citizenhex.com>
2019-09-09 16:45:38 -0400

    No Feed until commands fixed

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -45,7 +45,7 @@ function Main({location}){
             </Row>
         </Col>
         <Col md='1' className='feed'>
-          <Feed />
+          {/*<Feed />*/}
         </Col>
       </Row>
     </>
