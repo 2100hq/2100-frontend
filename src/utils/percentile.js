5ed58829227dd5add552947c113907aa0a9eb05a
Brett <brett@citizenhex.com>
2019-09-17 15:16:03 -0400

    get  percentile doesnt resort

--- src/utils/percentile.js
+++ src/utils/percentile.js
@@ -1,11 +1,11 @@
 import { BigNumber } from './index'
-export default function percentile(arr = [], p) {
+export default function percentile(arr = [], p, sorted = false) {
   if (arr.length === 0) return 0
   p = BigNumber(p).div(100)
   if (p.lte(0)) return arr[0]
   if (p.gte(1)) return arr[arr.length - 1]
 
-  arr.sort(function (a, b) { return BigNumber(a).minus(BigNumber(b)).gt(0) ? 1 : -1 })
+  if (!sorted) arr.sort(function (a, b) { return BigNumber(a).minus(BigNumber(b)).gt(0) ? 1 : -1 })
 
   const index = (arr.length - 1) * p
   const lower = Math.floor(index)
