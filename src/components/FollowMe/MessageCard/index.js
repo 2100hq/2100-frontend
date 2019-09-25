7341cf1ce82deb09a8d15415bf86852d92a790cc
ben <ben@citizenhex.com>
2019-09-25 14:40:42 -0400

    added two stage share button css, changed copy and icon

--- src/components/FollowMe/MessageCard/index.js
+++ src/components/FollowMe/MessageCard/index.js
@@ -359,8 +359,7 @@ export default function MessageCard({message, myToken, token, isSignedIn, action
         <div style={{width: '10%', float: 'right'}}>
           <a href='#' onClick={clickHandler(()=>history.push(`/$${token.name}`))}><ProfileImage token={token} /></a>
           <div className="small message-copy-url" onClick={clickHandler(postTweet)}>
-            <i class="fas fa-external-link-alt"></i>
-            <span>Share</span>
+            <i class="fab fa-twitter"></i><span> tweet</span>
           </div>
         </div>
         <div className='message-content' style={{width: '90%', float: 'left'}}>
