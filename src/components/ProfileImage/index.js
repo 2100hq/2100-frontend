0bed28bdb66e8b25b49923c6431a0a7483a2cca4
ben <ben@citizenhex.com>
2019-09-26 21:28:10 -0400

    added fluid image class from bootstrap to profile images

--- src/components/ProfileImage/index.js
+++ src/components/ProfileImage/index.js
@@ -6,5 +6,5 @@ const onError = e => e.target.src=url('0x00000')
 
 export default function ProfileImage({token, className='', style={} }){
   const name = typeof token === 'string' ? token : token.name
-  return <img className={'profile-image '+className} alt={`$${name}`} title={`$${name}`} style={style} src={url(name)} onError={onError} />
+  return <img className={'img-fluid profile-image '+className} alt={`$${name}`} title={`$${name}`} style={style} src={url(name)} onError={onError} />
 }
