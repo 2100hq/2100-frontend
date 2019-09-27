8eab8e309b67da7f5bf1fec0114383e2d34b2628
ben <ben@citizenhex.com>
2019-09-27 14:44:24 -0400

    first pass at better profile area

--- src/components/ProfileHeader/index.js
+++ src/components/ProfileHeader/index.js
@@ -1,22 +1,52 @@
 import React from 'react'
 import ProfileImage from '../ProfileImage'
 import { toDecimals } from '../../utils'
+import {Row, Col} from 'react-bootstrap'
+import './style.scss'
 
 export default function ProfileHeader({token}){
   return (
-    <div className='profile-header align-items-center justify-content-center'>
-      <div className='col-auto'>
-          <ProfileImage token={token}/>
-          <div className='token-stakes'>
-            <img src='/img/dai.png' style={{ width: '16px','vertical-align': 'baseline' }} /> {toDecimals(token.totalStakes)} staking
+    <div className='profile-header'>
+      <Row className='align-items-center'>
+        <Col md='7' className='image-area'>
+          <div className='module'>
+            <ProfileImage token={token}/>
           </div>
-          <div className='token-name'>
-            {token.name}
-          </div>
-          <div className='token-url small text-muted'>
-            <a href={`https://twitter.com/${token.name}`}>{`twitter.com/${token.name}`}</a>
-          </div>
-      </div>
+            <div className='token-name mt-2'>
+              {token.name}
+            </div>
+            <div className='token-stakes'>
+              <img src='/img/dai.png' style={{ width: '16px','vertical-align': 'baseline' }} /> {toDecimals(token.totalStakes, 2)} staking
+            </div>
+            <div className='token-url small mt-2'>
+              <a className='text-muted' href={`https://twitter.com/${token.name}`}><i class="fab fa-twitter"></i> {`${token.name}`}</a>
+            </div>
+        </Col>
+        <Col md='5' className='stakeholders-area'>
+
+            <div className='top-holders'>
+                <div className='top-label'>Top Holders</div>
+                <ProfileImage token='brttb'/>
+                <ProfileImage token='dapp_boi'/>
+                <ProfileImage token='richmcateer'/>
+                <ProfileImage token='stefancoolican'/>
+                <ProfileImage token='nanexcool'/>
+                <ProfileImage token='brianmosoff'/>
+                <ProfileImage token='austingriffith'/>
+            </div>
+            <hr/>
+            <div className='top-stakers'>
+                <div className='top-label'>Top Stakers</div>
+                <ProfileImage token='brttb'/>
+                <ProfileImage token='dapp_boi'/>
+                <ProfileImage token='richmcateer'/>
+                <ProfileImage token='stefancoolican'/>
+                <ProfileImage token='nanexcool'/>
+                <ProfileImage token='brianmosoff'/>
+                <ProfileImage token='austingriffith'/>
+            </div>
+        </Col>
+      </Row>
     </div>
   )
 }
