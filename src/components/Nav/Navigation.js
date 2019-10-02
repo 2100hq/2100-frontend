fa440f67116a45c10e3757975638bac37e2080a6
Brett <brett@citizenhex.com>
2019-10-02 11:54:01 -0400

    Navigation shows badge for number of decodables

--- src/components/Nav/Navigation.js
+++ src/components/Nav/Navigation.js
@@ -1,8 +1,10 @@
 import React, { useMemo } from 'react'
 import { compact } from 'lodash'
 import { useStoreContext } from '../../contexts/Store'
+import { useFollowMeContext } from '../../contexts/FollowMe'
 import { Nav } from 'react-bootstrap'
 import history from '../../utils/history'
+import {Badge} from 'react-bootstrap'
 import './style.scss'
 function Tab({current, name, set}){
   function handleClick(e){
@@ -17,25 +19,9 @@ function Tab({current, name, set}){
   )
 }
 
-function getViewName(viewType){
-  switch(viewType){
-    case 'Cheap':
-      return <span>Low Cost</span>
-    case 'Premium':
-      return <span>Premium</span>
-    case 'New':
-      return <span>New</span>
-    case 'Following':
-      return <span>Following</span>
-    case 'Gifts':
-      return <span>Gifts</span>
-    default:
-      return viewType
-  }
-}
-
 export default function Navigation(){
   const {state, query, dispatch, actions} = useStoreContext()
+  const { messages } = useFollowMeContext()
   const isSignedIn = query.getIsSignedIn()
   const currentView = query.getCurrentView()
 
@@ -56,6 +42,18 @@ export default function Navigation(){
   if (allowedViews.length < 2) return null // don't need to show tabs if there's only one possible view
 
   const tabs = allowedViews.map( viewType => <Nav.Link eventKey={viewType} key={viewType}>{getViewName(viewType)}</Nav.Link> )
+
+
+  function getViewName(viewType){
+    switch(viewType){
+      case 'Decodable':
+        const number = Object.values(messages||{}).filter(m=>m.decodable).length
+        const badge = number > 0 ? <Badge>{number}</Badge> : null
+        return <span>Decodable {badge}</span>
+      default:
+        return viewType
+    }
+  }
 
   return (
     <Nav className='fm-nav small' activeKey={currentView} defaultActiveKey={currentView} onSelect={setView}>
