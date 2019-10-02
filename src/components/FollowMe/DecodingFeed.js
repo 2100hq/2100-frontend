186c096a81a98648adb119405435ef4fc62379cd
Brett <brett@citizenhex.com>
2019-10-02 11:53:39 -0400

    fixed Decoding feed with new decodable flag

--- src/components/FollowMe/DecodingFeed.js
+++ src/components/FollowMe/DecodingFeed.js
@@ -9,23 +9,15 @@ export default function DecodingFeed({onChangePage}){
   const {query} = useStoreContext()
   let {messages, isSignedIn} = useFollowMeContext()
 
-  const tokens = query.getMyStakedOrHeldTokensArray()
-  const tokensHash = tokens.map(t => t.id+t.hasBalance+t.isStaking).join('')
-
-  const groupedTokens = useMemo( ()=> groupBy(tokens, token => token.isStaking ? 'isStaking' : 'hasBalance'), [tokensHash])
+  const tokens = query.getTokens()
+  const tokensHash = Object.values(tokens).map(t => t.id+t.balances.available+t.isStaking).join('')
 
   const messagesList = useMemo(()=>Object.values(messages), [messages,isSignedIn,tokensHash])
   const messagesHash = useMemo(()=>messagesList.map(m => m.id+m.hidden).join(''),[messagesList])
-  const shownMessages = useMemo(()=>messagesList.filter(m => {
-    if (!m.hidden && !m.decoded) return false // shown post hasn't recently been decoded
-    if ((groupedTokens.isStaking||[]).map(t=>t.id).includes(m.tokenid)) return true // if staking, you are decoding
-
-    const wallet = (groupedTokens.hasBalance||[]).find(t=>t.id===m.tokenid)
-
-    if (!wallet) return false // dont have any balance for this token
-
-    return BigNumber(wallet.balances.available).gte(m.threshold) // balance vs threshold to see the messasge. If you're not staking, only show messages where you're above the balance to see
-
+  const shownMessages = useMemo(()=>messagesList.filter(message => {
+    if (!message.hidden && !message.decoded) return false // shown post hasn't recently been decoded
+    if (tokens[message.tokenid] && tokens[message.tokenid].isStaking) return true
+    return message.decodeable
   }),[messagesHash, tokensHash])
 
   return <FollowMe messages={shownMessages} className='decoding-feed' onChangePage={onChangePage}/>
