63ecb0e89fe4df2034599cb40ceb97fa9d6d6f55
Brett <brett@citizenhex.com>
2019-10-03 14:26:34 -0400

    remove % change

--- src/components/Discover/All/index.js
+++ src/components/Discover/All/index.js
@@ -100,9 +100,6 @@ function Row ({ token, myToken, currentUsername, isAllocating, isEditing,  setIs
         <div className="col-3" style={{overflow: 'hidden'}}>
           <LinkableName token={token} />
         </div>
-        <div className="col-2" style={{overflow: 'hidden'}}>
-          +0.00 %
-        </div>
         <div style={{cursor: 'pointer'}} className="col-2">
           <span className='my-stake'><CountUp balance={myStake} decimals={2} /></span><span className='total-stakes'> / { token.totalStakes !== "0" ? <CountUp balance={totalStakes} decimals={2} /> : "0.00" }</span>
         </div>
@@ -168,7 +165,6 @@ function All({tokens = [], location, myToken, isAllocating, isEditing, setIsEdit
         <div className="col-4 asset-search">
           <i className="fas fa-search" /><input type='text' value={rawAssetSearch} onChange={setAssetSearch}/>
         </div>
-        <div className="col-2">% Change</div>
         <div className="col-3">Me / Total</div>
         <div className="col-2">Balance</div>
       </div>
