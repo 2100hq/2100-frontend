adc36c6048f86a689c7bc65380ebe9babeda7035
Brett <brett@citizenhex.com>
2019-10-03 13:03:27 -0400

    New feed shows my tokens

--- src/components/FollowMe/NewFeed.js
+++ src/components/FollowMe/NewFeed.js
@@ -8,10 +8,11 @@ export default function NewFeed({onChangePage}){
   const {query} = useStoreContext()
   const isSignedIn = query.getIsSignedIn()
   const { messages } = useFollowMeContext()
+  const myToken = query.getMyToken() || {}
   const messagesList = useMemo(()=>Object.values(messages), [messages,isSignedIn])
   const messagesHash = useMemo(()=>messagesList.map(m => m.id+m.hidden+m.decoded).join(''),[messagesList])
-  const shownMessages = useMemo(()=>messagesList.filter(message => !isSignedIn || message.hidden || message.decoded),[messagesHash])
+  const shownMessages = useMemo(()=>messagesList.filter(message => !isSignedIn || message.hidden || message.decoded || message.tokenid === myToken.id),[messagesHash])
   return (
-      <FollowMe messages={shownMessages} className='new-feed' onChangePage={onChangePage}/>
+      <FollowMe messages={messagesList} className='new-feed' onChangePage={onChangePage}/>
   )
 }
